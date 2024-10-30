afe2075a46ab68a2cdaf27c9173c7a0e2caada4b
ntnon <Anton.nydal@hotmail.com>
2024-10-30 01:32:41 +0100

    Update line-rider-synchronized-riders-mod.js

--- line-rider-synchronized-riders-mod.js
+++ line-rider-synchronized-riders-mod.js
@@ -1,388 +1,294 @@
 // ==UserScript==
-// @name         Line Rider Selection Rotate and Scale Mod
-// @namespace    http://tampermonkey.net/
-// @version      0.4
-// @description  Adds ability to rotate and scale selections
-// @author       David Lu
+
+// @name         Bosh spawn
+// @namespace    https://www.linerider.com/
+// @author       Anton
+// @description  Bosh placer mod
+// @version      1.0.1
+// @icon         https://www.linerider.com/favicon.ico
+
 // @match        https://www.linerider.com/*
 // @match        https://*.official-linerider.com/*
-// @match        http://localhost:8000/*
-// @downloadURL  https://github.com/EmergentStudios/linerider-userscript-mods/raw/master/selection-scale-rotate.user.js
+// @match        http://localhost:*/*
+// @match        https://*.surge.sh/*
+
+
+
+// @downloadURL  _
+// @updateURL    _
+// @homepageURL  _
+// @supportURL   _
 // @grant        none
+
 // ==/UserScript==
 
-// jshint asi: true
-// jshint esversion: 6
-
-/* constants */
-const SELECT_TOOL = 'SELECT_TOOL'
-const EMPTY_SET = new Set()
-const LINE_WIDTH = 2
-
-/* actions */
-const setTool = (tool) => ({
-  type: 'SET_TOOL',
-  payload: tool
-})
-
-const updateLines = (linesToRemove, linesToAdd) => ({
-  type: 'UPDATE_LINES',
-  payload: { linesToRemove, linesToAdd }
-})
-
-const setLines = (line) => updateLines(null, line)
+/* global Actions, Selectors */
 
 const commitTrackChanges = () => ({
-  type: 'COMMIT_TRACK_CHANGES'
-})
+    type: "COMMIT_TRACK_CHANGES"
+});
 
 const revertTrackChanges = () => ({
-  type: 'REVERT_TRACK_CHANGES'
-})
+    type: "REVERT_TRACK_CHANGES"
+});
 
-const setEditScene = (scene) => ({
-  type: 'SET_RENDERER_SCENE',
-  payload: { key: 'edit', scene }
-})
+const setRiders = (new_riders) => ({ type: "SET_RIDERS", payload: new_riders })
 
-/* selectors */
-const getActiveTool = state => state.selectedTool
-const getToolState = (state, toolId) => state.toolState[toolId]
-const getSelectToolState = state => getToolState(state, SELECT_TOOL)
-const getSimulatorCommittedTrack = state => state.simulator.committedEngine
-const getEditorZoom = state => state.camera.editorZoom
+const getSimulatorCommittedTrack = state => state.simulator.committedEngine;
 
-class ScaleRotateMod {
-  constructor (store, initState) {
-    this.store = store
+const getSimulatorCommittedRiders = state => getSimulatorCommittedTrack(state).engine.state.riders
 
-    this.changed = false
-    this.state = initState
 
-    this.track = getSimulatorCommittedTrack(this.store.getState())
-    this.selectedPoints = EMPTY_SET
 
-    store.subscribeImmediate(() => {
-      this.onUpdate()
-    })
-  }
 
-  commit () {
-    if (this.changed) {
-      this.store.dispatch(commitTrackChanges())
-      this.store.dispatch(revertTrackChanges())
-      this.store.dispatch(setEditScene(new Millions.Scene()))
-      this.changed = false
-      return true
-    }
-  }
 
-  onUpdate (nextState = this.state) {
-    let shouldUpdate = false
 
-    if (this.state !== nextState) {
-      this.state = nextState
-      shouldUpdate = true
+class RiderMod {
+    constructor(store, initState) {
+        this.store = store;
+        this.state = initState;
+
+        this.track = getSimulatorCommittedTrack(store.getState());
+        /* Substate Variables */
+
+        this.changed = false;
+
+        store.subscribeImmediate(() => {
+            this.onUpdate();
+        });
     }
 
-    if (this.state.active) {
-      const track = getSimulatorCommittedTrack(this.store.getState())
-      if (this.track !== track) {
-        this.track = track
-        shouldUpdate = true
-      }
+    //update /fix
+    remove() {
+        if (this.changed) return false;
+        // Create a copy of the current riders array
+        const newRiders = getSimulatorCommittedRiders(this.store.getState()).pop()
+        // Remove the last rider from the copied array
 
-      const selectToolState = getSelectToolState(this.store.getState())
-
-      let selectedPoints = selectToolState.selectedPoints
-
-      if (!selectToolState.multi) {
-        selectedPoints = EMPTY_SET
-      }
-
-      if (!setsEqual(this.selectedPoints, selectedPoints)) {
-        this.selectedPoints = selectedPoints
-        shouldUpdate = true
-      }
+        // Dispatch the updated array of riders
+        this.store.dispatch(setRiders(newRiders));
+        this.riders = [];
+        this.changed = true;
     }
 
-    if (shouldUpdate) {
-      if (this.changed) {
-        this.store.dispatch(revertTrackChanges())
-        this.store.dispatch(setEditScene(new Millions.Scene()))
-        this.changed = false
-      }
 
-      if (this.state.active && this.selectedPoints.size > 0 && (this.state.scale !== 1 || this.state.scaleX !== 1 || this.state.scaleY !== 1 || this.state.rotate !== 0)) {
-        const selectedLines = [...getLinesFromPoints(this.selectedPoints)]
-          .map(id => this.track.getLine(id))
-          .filter(l => l)
+    commit() {
+        if (!this.changed) return false;
+        this.store.dispatch(commitTrackChanges());
+        this.store.dispatch(revertTrackChanges());
+        this.changed = false;
+        return true;
+    }
 
-        const {x, y, width, height} = getBoundingBox(selectedLines)
-        const c = new V2({
-          x: x + width / 2,
-          y: y + height / 2
-        })
+    onUpdate(nextState = this.state) {
+        // Helper variable to check if the mod should update
+        let shouldUpdate = false;
 
-        const transform = this.getTransform()
-        const transformedLines = []
-
-        for (let line of selectedLines) {
-          const p1 = new V2(line.p1).sub(c).transform(transform).add(c)
-          const p2 = new V2(line.p2).sub(c).transform(transform).add(c)
-
-          transformedLines.push({
-            ...line.toJSON(),
-            x1: p1.x,
-            y1: p1.y,
-            x2: p2.x,
-            y2: p2.y
-          })
+        // Sets the line preview mode to fast select
+        if (!this.state.active && nextState.active) {
+            window.previewLinesInFastSelect = true;
+        }
+        if (this.state.active && !nextState.active) {
+            window.previewLinesInFastSelect = false;
         }
 
-        this.store.dispatch(setLines(transformedLines))
-
-        const zoom = getEditorZoom(this.store.getState())
-        const renderedBox = genBoxOutline(x, y, x + width, y + height, 1 / zoom, new Millions.Color(0, 0, 0, 255), 0)
-
-        for (let line of renderedBox) {
-          const p1 = new V2(line.p1).sub(c).transform(transform).add(c)
-          const p2 = new V2(line.p2).sub(c).transform(transform).add(c)
-          line.p1.x = p1.x
-          line.p1.y = p1.y
-          line.p2.x = p2.x
-          line.p2.y = p2.y
+        // Checks whether the mod state itself has changed
+        if (this.state !== nextState) {
+            this.state = nextState;
+            shouldUpdate = true;
         }
-        this.store.dispatch(setEditScene(Millions.Scene.fromEntities(renderedBox)))
+
+        // Checks that the engine has changed, only if the mod is active
+        if (this.state.active) {
+            const track = getSimulatorCommittedTrack(this.store.getState());
+
+            if (this.track !== track) {
+                this.track = track;
+                shouldUpdate = true;
+            }
+        }
+
+        // Don't need to do anything if there aren't updates
+        if (!shouldUpdate) return;
+
+        // If changes have been made previously, discard them for the new changes incoming
+        if (this.changed) {
+            this.store.dispatch(revertTrackChanges());
+            this.changed = false;
+        }
+
+        // If the mod isn't active, then no new changes are incoming and the previous changes have
+        // already been discard, so the function is done
+        if (!this.state.active) return;
+        if (!shouldUpdate) return;
+
+
+        if (this.changed) {
+            this.store.dispatch(revertTrackChanges());
+            this.changed = false;
+        }
+
+        if (!this.state.active) return;
+
+        const committed_riders = getSimulatorCommittedRiders(this.store.getState())
+
+
+        const new_riders = []
+
+        for (let i = 0; i < this.state.rider_count; i++) {
+            new_riders.push({
+                "startPosition": {
+                    "x": this.state.x + (i * this.state.x_offset),
+                    "y": this.state.y + (i * this.state.y_offset)
+                },
+                "startVelocity": {
+                    "x": this.state.x_velocity,
+                    "y": this.state.y_velocity
+                },
+                "remountable": 1,
+                "createdUsingMod": true
+
+            })
+        }
+
+        this.store.dispatch(setRiders([...committed_riders, ...new_riders]))
         this.changed = true
-      }
     }
-  }
-
-  getTransform() {
-    const transform = rotateTransform(this.state.rotate * Math.PI / 180)
-    transform[0] *= this.state.scale
-    transform[3] *= this.state.scale
-    transform[0] *= this.state.scaleX
-    transform[3] *= this.state.scaleY
-    return transform
-  }
 }
 
-function main () {
-  const {
-    React,
-    store
-  } = window
+function main() {
+    const {
+        React,
+        store
+    } = window;
+    const create = React.createElement;
 
-  const e = React.createElement
+    class RiderModComponent extends React.Component {
+        constructor(props) {
+            super(props);
 
-  class ScaleRotateModComponent extends React.Component {
-    constructor (props) {
-      super(props)
+            this.state = {
+                active: false,
+                x: 0,
+                y: 0,
+                x_velocity: 0.4,
+                y_velocity: 0,
+                rider_count: 1,
+                x_offset: 10,
+                y_offset: 0
+            };
 
-      this.state = {
-        active: false,
-        scale: 1,
-        scaleX: 1,
-        scaleY: 1,
-        rotate: 0,
-      }
+            this.mod = new RiderMod(store, this.state);
 
-      this.scaleRotateMod = new ScaleRotateMod(store, this.state)
+            store.subscribe(() => {
 
-      store.subscribe(() => {
-        const selectToolActive = getActiveTool(store.getState()) === SELECT_TOOL
-
-        if (this.state.active && !selectToolActive) {
-          this.setState({ active: false })
+            });
         }
-      })
 
-      this.onCommit = () => {
-        this.scaleRotateMod.commit()
-        this.setState({
-          scale: 1,
-          scaleX: 1,
-          scaleY: 1,
-          rotate: 0
-        })
-      }
-      this.onMouseCommit = () => {
-        this.onCommit()
-        window.removeEventListener('mouseup', this.onMouseCommit)
-      }
-      this.onKeyCommit = e => {
-        if (e.key === 'Enter') {
-          this.onCommit()
+        componentWillUpdate(nextProps, nextState) {
+            this.mod.onUpdate(nextState);
         }
-      }
-    }
 
-    componentWillUpdate (nextProps, nextState) {
-      this.scaleRotateMod.onUpdate(nextState)
-    }
+        onActivate() {
+            if (this.state.active) {
+                this.setState({ active: false });
+            } else {
+                this.setState({ active: true });
+            }
+        }
 
-    onActivate () {
-      if (this.state.active) {
-        this.setState({ active: false })
-      } else {
-        store.dispatch(setTool(SELECT_TOOL))
-        this.setState({ active: true })
-      }
-    }
+        onCommit() {
+            const committed = this.mod.commit();
 
-    renderSlider (key, props) {
-      props = {
-        ...props,
-        value: this.state[key],
-        onChange: e => this.setState({ [key]: parseFloatOrDefault(e.target.value) })
-      }
-      const rangeProps = {
-        ...props,
-        onMouseDown: () => window.addEventListener('mouseup', this.onMouseCommit)
-      }
-      const numberProps = {
-        ...props,
-        onKeyUp: this.onKeyCommit,
-        onBlur: this.onCommit
-      }
-      return e('div', null,
-        key,
-        e('input', { style: { width: '3em' }, type: 'number', ...numberProps }),
-        e('input', { type: 'range', ...rangeProps, onFocus: e => e.target.blur() })
-      )
-    }
+            if (committed) {
+                this.setState({ active: false });
+            }
+        }
+        onRemove() {
+            this.mod.remove()
+        }
+        onAdd() {
+            this.mod.add()
+        }
 
-    render () {
-      return e('div',
-        null,
-        this.state.active && e('div', null,
-          this.renderSlider('scaleX', { min: 0, max: 2, step: 0.01 }),
-          this.renderSlider('scaleY', { min: 0, max: 2, step: 0.01 }),
-          this.renderSlider('scale', { min: 0, max: 2, step: 0.01 }),
-          this.renderSlider('rotate', { min: -180, max: 180, step: 1 })
-        ),
-        e('button',
-          {
-            style: {
-              backgroundColor: this.state.active ? 'lightblue' : null
-            },
-            onClick: this.onActivate.bind(this)
-          },
-          'Scale Rotate Mod'
-        )
-      )
-    }
-  }
+        renderSlider(key, title, props) {
+            props = {
+                ...props,
+                value: this.state[key],
+                onChange: e => this.setState({ [key]: parseFloat(e.target.value) })
+            };
 
-  // this is a setting and not a standalone tool because it extends the select tool
-  window.registerCustomSetting(ScaleRotateModComponent)
+            return create("div", null,
+                title,
+                create("input", { style: { width: "3em" }, type: "number", ...props }),
+                create("input", { type: "range", ...props, onFocus: e => e.target.blur() })
+            );
+        }
+
+        renderNumber(key, title, props) {
+            props = {
+                ...props,
+                value: this.state[key],
+                onChange: e => this.setState({ [key]: parseFloat(e.target.value) })
+            };
+
+            return create("div", null,
+                title,
+                create("input", { style: { width: "3em" }, type: "number", ...props }),
+            );
+        }
+
+        render() {
+            return create("div", null,
+                this.state.active && create("div", null,
+                    /* Mod UI */
+                    create("div", null,
+                        this.renderNumber("rider_count", "amount", { min: -40, max: 40, step: 1 }),
+                        this.state.rider_count > 1 && create("div", null,
+                            this.renderSlider("x_offset", "x offset", { min: -40, max: 40, step: 1 }),
+                            this.renderSlider("y_offset", "y offset", { min: -40, max: 40, step: 1 })
+                        ),
+                        this.renderSlider("x", "X", { min: -40, max: 40, step: 1 }),
+                        this.renderSlider("y", "Y", { min: -40, max: 40, step: 1 }),
+                        this.renderSlider("x_velocity", "x velocity", { min: -40, max: 40, step: 1 }),
+                        this.renderSlider("y_velocity", "y velocity", { min: -40, max: 40, step: 1 })
+                    ),
+                    create("button",
+                        {
+                            style: { float: "left" },
+                            onClick: this.onRemove.bind(this)
+                        },
+                        "Remove Rider"
+                    ),
+                    create("button",
+                        {
+                            style: { float: "left" },
+                            onClick: this.onCommit.bind(this)
+                        },
+                        "Commit"
+                    )
+                ),
+                create("button",
+                    {
+                        style: { backgroundColor: this.state.active ? "lightblue" : null },
+                        onClick: this.onActivate.bind(this)
+                    },
+                    "Bosh spawner"
+                )
+            );
+        }
+    }
+    window.registerCustomSetting(RiderModComponent);
 }
 
-/* init */
 if (window.registerCustomSetting) {
-  main()
+    main();
 } else {
-  const prevCb = window.onCustomToolsApiReady
-  window.onCustomToolsApiReady = () => {
-    if (prevCb) prevCb()
-    main()
-  }
+    const prevCb = window.onCustomToolsApiReady;
+    window.onCustomToolsApiReady = () => {
+        if (prevCb) prevCb();
+        main();
+    };
 }
 
-/* utils */
-function setsEqual (a, b) {
-  if (a === b) {
-    return true
-  }
-  if (a.size !== b.size) {
-    return false
-  }
-  for (let x of a) {
-    if (!b.has(x)) {
-      return false
-    }
-  }
-  return true
-}
-
-function getLinesFromPoints (points) {
-  return new Set([...points].map(point => point >> 1))
-}
-
-function rotateTransform (rads) {
-  const { V2 } = window
-
-  let u = V2.from(1, 0).rot(rads)
-  let v = V2.from(0, 1).rot(rads)
-
-  return [u.x, v.x, u.y, v.y, 0, 0]
-}
-
-function parseFloatOrDefault (string, defaultValue = 0) {
-  const x = parseFloat(string)
-  return isNaN(x) ? defaultValue : x
-}
-
-function getBoundingBox (lines) {
-  if (lines.size === 0) {
-    return {
-      x: 0,
-      y: 0,
-      width: 0,
-      height: 0
-    }
-  }
-  let minX = Infinity
-  let minY = Infinity
-  let maxX = -Infinity
-  let maxY = -Infinity
-
-  for (let line of lines) {
-    minX = Math.min(line.p1.x, minX)
-    minY = Math.min(line.p1.y, minY)
-    maxX = Math.max(line.p1.x, maxX)
-    maxY = Math.max(line.p1.y, maxY)
-
-    minX = Math.min(line.p2.x, minX)
-    minY = Math.min(line.p2.y, minY)
-    maxX = Math.max(line.p2.x, maxX)
-    maxY = Math.max(line.p2.y, maxY)
-  }
-
-  return {
-    x: minX,
-    y: minY,
-    width: maxX - minX,
-    height: maxY - minY
-  }
-}
-
-function genLine (x1, y1, x2, y2, thickness, color, zIndex) {
-  let p1 = {
-    x: x1,
-    y: y1,
-    colorA: color,
-    colorB: color,
-    thickness
-  }
-  let p2 = {
-    x: x2,
-    y: y2,
-    colorA: color,
-    colorB: color,
-    thickness
-  }
-  return new Millions.Line(p1, p2, 3, zIndex)
-}
-
-
-function genBoxOutline (x1, y1, x2, y2, thickness, color, zIndex) {
-  return [
-    genLine(x1, y1, x1, y2, thickness, color, zIndex),
-    genLine(x1, y2, x2, y2, thickness, color, zIndex + 0.1),
-    genLine(x2, y2, x2, y1, thickness, color, zIndex + 0.2),
-    genLine(x2, y1, x1, y1, thickness, color, zIndex + 0.3)
-  ]
-}
+/* Utility Functions */

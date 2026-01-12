6b8afbea95241e97638806d70dabbd13b5372922
Anton Nydal <anton@tl87b.local>
2026-01-12 22:26:08 +0100

    initialize

---
+++ mods/line-rider-gravity-api.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+
+// @name         RiderManager API
+// @namespace    https://www.linerider.com/
+// @author       Anton Nydal
+// @description  API for advanced rider management system for Line Rider
+// @version      1.0
+// @icon         https://www.linerider.com/favicon.ico
+
+// @match        https://www.linerider.com/*
+// @match        https://*.official-linerider.com/*
+// @match        http://localhost:*/*
+// @match        https://*.surge.sh/*
+
+// @downloadURL  _
+// @updateURL    _
+// @homepageURL  _
+// @supportURL   _
+// @grant        none
+
+// ==/UserScript==
